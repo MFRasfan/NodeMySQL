1ca22a0bffefc5f9db0c323264be2dec201c373c
Rasfan M Faiz <rasfanfaiz@gmail.com>
2019-09-01 13:35:10 -0400

    completedcode

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # NodeMySQL
+
+This is a command line app that Helps you Buy a Product.
+
+MySQL Database with Products 
+var mysql = require('mysql');
+var inquirer = require('inquirer')
+
+You need these to run this app succesfully on the Terminal. 
+
+Commands
+
+1 . To Buy A Product
+
+Run on Terminal 
+
+node bamazonCustomer.js
+
+
+![Alt text](/Images/Whattobuy)
+
+ 
+
+2 . Enter the Product You want to Buy
+
+Type on Terminal 
+
+Type the Product Name from the Populated List
+
+
+ ![Alt text](/Images/Whattobuy)
+
+
+3 . Choose How Many You would Like to Buy
+
+Type on Terminal 
+
+Type the Quantity
+
+![Alt text](/Images/Howmanytobuy)
+
+3 . Finally You will see The Total Cost of What you Bought
+
+Display on Terminal 
+
+Total Cost
+
+![Alt text](/Images/totalPrice)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,4 +9,108 @@ var connection = mysql.createConnection({
     user: "root",
     password: "password",
     database: "bamazon_db"
-})
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    create();
+  });
+
+
+  var create = function () {
+    //display data for user 
+    connection.query("SELECT * FROM products", function (err, res) {
+
+        for (var i = 0; i < res.length; i++) {
+
+            var Results = [
+                
+                "ID:" + res[i].item_id,
+                "Product:" + res[i].product_name,
+                "Price:" + res[i].price,
+                
+            ].join("-");
+
+            
+            console.log(Results); 
+            
+        }
+        //ask user if they want to buy a product listed
+        prompt(res);
+    })
+}
+
+var prompt = function (res) {
+    //take user input for question 1
+    inquirer.prompt([{
+        type: 'input',
+        name: 'choice',
+        message: "What Would You Like To Buy?"
+    }])
+
+.then(function (answer) {
+    var correct = false;
+    
+        //if input matches item listed as next question
+        for (var i = 0; i < res.length; i++) {
+            if (res[i].product_name == answer.choice) {
+                correct = true;
+                var product = answer.choice;
+                var id = i;
+                inquirer.prompt({
+                    type: "input",
+                    name: "quant",
+                    message: "How Many Would You Like to Buy?",
+                    validate: function (value) {
+                        if (isNaN(value) == false) {
+                            return true;
+
+                        } else {
+                            return false;
+                        }
+                    }
+                    //If the stockquanitity is not less then zero then minus amount ordered from the total 
+                })
+                
+                .then(function (answer) {
+                    if ((res[id].stock_quantity - answer.quant) > 0) {
+                        connection.query("UPDATE products SET stock_quantity='" + 
+                        (res[id].stock_quantity - answer.quant) + 
+                        "'WHERE product_name='" + product + "'", function () {
+
+                            create();
+                            var totalCost = answer.quant * res[id].price;
+
+                            var cResults = [
+                                
+                                "You Bought a Product",
+                                "TOTAL COST: $ " + totalCost,
+                            
+
+                            ].join("\n\n");
+
+                            
+                            console.log(cResults); 
+
+                            
+                        })
+                        //if zero quanitity
+                    } else {
+                        console.log("Insufficient quantity!" + "\n");
+                        prompt(res);
+                    }
+
+                })
+            }
+        }
+        //If input does not match
+        if (i == res.length && correct == false) {
+            console.log("Wrong Choice!" + "\n");
+            prompt(res);
+        }
+    })
+}
+
+
+
+
